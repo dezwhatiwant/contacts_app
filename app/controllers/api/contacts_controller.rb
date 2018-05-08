886825baa108e931a234af3f12899ad8db2dce7e
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-07 21:28:33 -0500

    modified controller, added new url

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,14 +1,13 @@
 class Api::ContactsController < ApplicationController
   def one_contact_action
     @contact = Contact.first
-    render 'one_contact_view.json.jbuilder'
-    
+    render 'one_contact_view.json.jbuilder' 
   end
 
   def every_contact
     @contacts = Contact.all
     render 'belonging_to_us.json.jbuilder'
-    
+
     
   end
 end

35dfee8b0f892df7b3efcaf0c58f7eee27a57040
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-10 21:21:06 -0500

    added show action

--- runner.rb
+++ runner.rb
@@ -1,6 +1,10 @@
 require 'unirest'
 
 # INDEX ACTION
-response = Unirest.get("http://localhost:3000/api/contacts")
-index_hash = response.body
-puts JSON.pretty_generate(index_hash)
+# response = Unirest.get("http://localhost:3000/api/contacts")
+# index_hash = response.body
+# puts JSON.pretty_generate(index_hash)
+
+#SHOW ACITON
+response = Unirest.get("http://localhost:3000/api/contacts/1")
+puts JSON.pretty_generate(response.body)

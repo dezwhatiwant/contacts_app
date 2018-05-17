5a4ca5d71ab917c3dd69f3dc5f7a1a12e317a550
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-16 21:35:51 -0500

    moved extras from database and added migration files and partials to include formatted attributes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
-contact = Contact.new(first_name: "Peter", last_name: "Parker", email: "webhead@spider.web", phone_number: "313555101")
-contact.save
 
-contact = Contact.new(first_name: "Will", last_name: "Smith", email: "iamlegend@gmail.com", phone_number: "8003155716")
-contact.save
 
-contact = Contact.new(first_name: "Steve", last_name: "Smith", email: "little@guy.com", phone_number: "911")
-contact.save
+50.times do
+
+  first_name = Faker::Name.first_name
+  middle_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  Contact.create(
+                first_name: first_name,
+                middle_name: middle_name,
+                last_name: last_name,
+                email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+                phone_number: Faker::PhoneNumber.phone_number
+                )
+
+end

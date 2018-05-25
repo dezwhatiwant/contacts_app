9905e29ac8c81e8dfc4989b18bf8178fa601d5b2
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-24 21:31:05 -0500

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+  post "/users" => "users#create"
+
   namespace :api do
   get '/contacts' => 'contacts#index'
   get '/contacts/:id' => 'contacts#show'

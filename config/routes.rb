48d6c9fa533d31beda6c8cae6c8fb071aa4ba151
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-27 14:05:31 -0500

    modified controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  post 'user_token' => 'user_token#create'
   post "/users" => "users#create"
+  post 'user_token' => 'user_token#create'
   
 
   namespace :api do

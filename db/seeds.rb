d1b790ac0f93711df8eabf5bb499432c1f37ef16
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-31 18:05:36 -0500

    added groups and contact groups join table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,23 @@
 
 
-50.times do
+# 50.times do
 
-  first_name = Faker::Name.first_name
-  middle_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
+#   first_name = Faker::Name.first_name
+#   middle_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
 
-  Contact.create(
-                first_name: first_name,
-                middle_name: middle_name,
-                last_name: last_name,
-                email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
-                phone_number: Faker::PhoneNumber.phone_number
-                )
+#   Contact.create(
+#                 first_name: first_name,
+#                 middle_name: middle_name,
+#                 last_name: last_name,
+#                 email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+#                 phone_number: Faker::PhoneNumber.phone_number
+#                 )
 
-end
+# end
+
+# Group.create(name: "Family")
+# Group.create(name: "Friends")
+# Group.create(name: "Owe Me Money")
+# Group.create(name: "Owed Money")
+# Group.create(name: "Fantasy Football")
